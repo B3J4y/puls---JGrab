32d10059f5cd178a64b93e03c75757a6cd9187a8
B3J4y <janbernoth@gmail.com>
2012-02-24 01:06:52 +0100

    changed mostly the regexpression to find the notes in the shedule of puls

--- Puls - Java/src/Login.java
+++ Puls - Java/src/Login.java
@@ -33,22 +33,24 @@ public class Login {
 //			URL url = new URL("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=B991BAC6341E5589790CD58A3910E40E.node11?state=verpublish&amp;status=init&amp;vmfile=no&amp;publishid=25196&amp;moduleCall=webInfo&amp;publishConfFile=webInfo&amp;publishSubDir=veranstaltung");
 			//Stundplan lang link
 //			URL url = new URL("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=B991BAC6341E5589790CD58A3910E40E.node11?state=wplan&week=-2&act=show&pool=&show=plan&P.vx=lang&P.subc=plan");
-			URL url = new URL("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=1A46247C2820F88217B918DADD4B1FC4.node11?state=wplan&value=43_2011&act=show&pool=&show=plan&P.vx=lang&P.subc=plan&week=51_2011");
+			//Wochenansicht
+//			URL url = new URL("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=1A46247C2820F88217B918DADD4B1FC4.node11?state=wplan&value=43_2011&act=show&pool=&show=plan&P.vx=lang&P.subc=plan&week=51_2011");
+			URL url = new URL("http://pro5.wi.uni-potsdam.de:8080/studiUpServer/puls");
 			HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-			connection.setRequestMethod( "POST" );
-			connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
-			connection.setRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
-			connection.setRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
-			connection.setRequestProperty("Accept-Language", "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4");
-			connection.setRequestProperty("Cache-Control", "max-age=0");
-			connection.setRequestProperty("Cache-Control", "max-age=0");
-			connection.setRequestProperty("Connection", "keep-alive");
-			connection.setRequestProperty("Content-Length", "29");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-			connection.setRequestProperty("Cookie", "__utma=221810396.706524385.1312495184.1312495184.1328097894.2; __utmz=221810396.1328097894.2.2.utmcsr=mentoring-brandenburg.de|utmccn=(referral)|utmcmd=referral|utmcct=/");
-			connection.setRequestProperty("Host", "puls.uni-potsdam.de");
-			connection.setRequestProperty("Origin" , "null");
-			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7");
+			connection.setRequestMethod( "GET" );
+//			connection.setRequestProperty("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+//			connection.setRequestProperty("Accept-Charset", "ISO-8859-1,utf-8;q=0.7,*;q=0.3");
+//			connection.setRequestProperty("Accept-Encoding", "gzip,deflate,sdch");
+//			connection.setRequestProperty("Accept-Language", "de-DE,de;q=0.8,en-US;q=0.6,en;q=0.4");
+//			connection.setRequestProperty("Cache-Control", "max-age=0");
+//			connection.setRequestProperty("Cache-Control", "max-age=0");
+//			connection.setRequestProperty("Connection", "keep-alive");
+//			connection.setRequestProperty("Content-Length", "29");
+//			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+//			connection.setRequestProperty("Cookie", "__utma=221810396.706524385.1312495184.1312495184.1328097894.2; __utmz=221810396.1328097894.2.2.utmcsr=mentoring-brandenburg.de|utmccn=(referral)|utmcmd=referral|utmcct=/");
+//			connection.setRequestProperty("Host", "puls.uni-potsdam.de");
+//			connection.setRequestProperty("Origin" , "null");
+//			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.7 (KHTML, like Gecko) Chrome/16.0.912.77 Safari/535.7");
 			OutputStream out = new FileOutputStream("res/index.html");
 			BufferedReader reader = new BufferedReader(
                     new InputStreamReader(connection.getInputStream()) );
@@ -57,9 +59,10 @@ public class Login {
 			for ( String line; (line = reader.readLine()) != null; )
 			{
 				output+=line;
+				System.out.println(line);
 			}
-			writer.writeFile(output,System.getProperty("user.home")
-					+ "\\Documents\\index.html");
+//			writer.writeFile(output,System.getProperty("user.home")
+//					+ "\\Documents\\le.xml");
 			
 		} catch (Exception e) {
 			e.printStackTrace();
--- Puls - Java/src/parsers/pulsParser.java
+++ Puls - Java/src/parsers/pulsParser.java
@@ -41,7 +41,7 @@ public class pulsParser {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new pulsParser("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=91DD7ED4CA25B13C1EBE47F5E05CEC92.node11?state=wplan&value=43_2011&act=show&pool=&show=plan&P.vx=lang&P.subc=plan&week=51_2011");
+		new pulsParser("https://puls.uni-potsdam.de/qisserver/rds;jsessionid=7ACD83ED4BAFD3B720EE1C0C3ABFE8DC.node11?state=wplan&value=43_2011&act=show&pool=&show=plan&P.vx=lang&P.subc=plan&week=51_2011");
 
 	}
 
